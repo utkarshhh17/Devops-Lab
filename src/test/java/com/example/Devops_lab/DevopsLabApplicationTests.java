65f180b269e8b2c076054c2c36a935b7ef095d78
utkarshhh17 <singhutkarsh.1210@gmail.com>
2024-06-09 19:31:59 +0530

    commit

--- src/test/java/com/example/Devops_lab/DevopsLabApplicationTests.java
+++ src/test/java/com/example/Devops_lab/DevopsLabApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.Devops_lab;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DevopsLabApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
+	void greetingShouldReturnDefaultMessage() {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+				String.class)).contains("Hello world");
 	}
+
+
 
 }
